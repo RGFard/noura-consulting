402a513b3485f2f34e0d47e97490d4ca3ecb0ce0
Reza <reza2011@gmail.com>
2023-05-26 15:56:00 -0400

    created deployment script

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,7 +7,7 @@ const Video = () => {
         <section className="vision">
             <video className="vision__video" autoPlay muted loop>
                 <source src={VisionVideo} type="video/mp4" />
-                Your browser is not supported! Open it with Chrome or Firefox.
+                Your browser is not supported!
             </video>
         </section>
     );
