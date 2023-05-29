068fec3df3878fec998c7a5eacc7ab56c2f9a927
Reza <reza2011@gmail.com>
2023-05-29 16:51:49 -0400

    revert icons back

--- package/src/components/SEO.js
+++ package/src/components/SEO.js
@@ -2,31 +2,31 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-// const query = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//         description
-//       }
-//     }
-//   }
-// `;
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
 
 const SEO = ({ title, description }) => {
-  // const { site } = useStaticQuery(query);
-  // const metaDescription = site.siteMetadata.description || description;
+  const { site } = useStaticQuery(query);
+  const metaDescription = site.siteMetadata.description || description;
   return (
-    // <Helmet
-    //   htmlAttributes={{ lang: "en" }}
-    //   title={`${title} | ${site.siteMetadata.title}`}
-    //   meta={[{ name: `description`, content: metaDescription }]}>
-    //   <link
-    //     href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i"
-    //     rel="stylesheet"
-    //   />
-    // </Helmet>
-    <div></div>
+    <Helmet
+      htmlAttributes={{ lang: "en" }}
+      title={`${title} | ${site.siteMetadata.title}`}
+      meta={[{ name: `description`, content: metaDescription }]}>
+      <link
+        href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i"
+        rel="stylesheet"
+      />
+    </Helmet>
+    // <div></div>
   );
 };
 
--- package/src/components/icons/Icon.js
+++ package/src/components/icons/Icon.js
@@ -2,26 +2,26 @@ import React from "react";
 import parser from "html-react-parser";
 import { useStaticQuery, graphql } from "gatsby";
 
-const query = graphql`
-{
-  allContentfulJsonContent {
-    nodes {
-      object {
-        name
-        tag
-      }
-    }
-  }
-}
-`;
+// const query = graphql`
+// {
+//   allContentfulJsonContent {
+//     nodes {
+//       object {
+//         name
+//         tag
+//       }
+//     }
+//   }
+// }
+// `;
 
-// const Icon = ({ name, icons }) => {
-const Icon = ({ name }) => {
+const Icon = ({ name, icons }) => {
+  // const Icon = ({ name }) => {
   let iconTag;
 
-  const {
-    allContentfulJsonContent: { nodes: icons }
-  } = useStaticQuery(query);
+  // const {
+  //   allContentfulJsonContent: { nodes: icons }
+  // } = useStaticQuery(query);
 
   icons.find(() => true).object.map((icon) => {
     if (icon.name === name) {
