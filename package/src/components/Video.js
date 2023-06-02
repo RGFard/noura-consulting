613817a4c03b95d7dc497f0854a8bfb0171ed9cc
Reza <reza2011@gmail.com>
2023-06-01 23:22:27 -0400

    adding video webm format

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-import VisionVideo from "../assets/images/vision.mp4";
+import VisionVideoMp4 from "../assets/images/vision.mp4";
+import VisionVideoWebm from "../assets/images/vision.mp4";
 
 const Video = () => {
     return (
         <section className="vision">
-            <video className="vision__video" contentType="video/mp4" autoPlay muted loop>
-                <source src={VisionVideo} type="video/mp4" />
+            <video className="vision__video" autoPlay muted loop>
+                <source src={VisionVideoMp4} type="video/mp4" />
+                <source src={VisionVideoWebm} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
         </section>
