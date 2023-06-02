70359dbcd9f93650d0ac7419e96a3233be4bcd1f
Reza <reza2011@gmail.com>
2023-06-02 19:13:14 -0400

    putting back mp4

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 import VisionVideoMp4 from "../assets/images/vision.mp4";
-import VisionVideoWebm from "../assets/images/vision.webm";
+import VisionVideoMov from "../assets/images/vision.mov";
 
 const Video = () => {
     return (
         <section className="vision">
             <video className="vision__video" autoPlay muted loop>
-                {/* <source src={VisionVideoMp4} type="video/mp4" /> */}
-                <source src={VisionVideoWebm} type="video/webm" />
+                <source src={VisionVideoMp4} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
         </section>
