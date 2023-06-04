e81d8de7f07a10b41eb29171152dd38e2236d755
Reza <reza2011@gmail.com>
2023-06-04 19:33:04 -0400

    shuffling video attributes

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -5,7 +5,7 @@ import VisionVideoMp4 from "../assets/images/vision.mp4";
 const Video = () => {
     return (
         <section className="vision">
-            <video className="vision__video" controls preload='auto' autoplay playsInline>
+            <video className="vision__video" controls preload='auto' autoplay loop muted playsInline>
                 <source src={VisionVideoMp4} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
