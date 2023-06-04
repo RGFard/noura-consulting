c13ac2f4850b6d0f582525e98ba6616d9aac0096
Reza <reza2011@gmail.com>
2023-06-04 16:54:38 -0400

    adding attribute for video to play

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -5,7 +5,7 @@ import VisionVideoMp4 from "../assets/images/vision.mp4";
 const Video = () => {
     return (
         <section className="vision">
-            <video className="vision__video" autoPlay muted loop>
+            <video className="vision__video" autoPlay muted loop playsinline>
                 <source src={VisionVideoMp4} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
