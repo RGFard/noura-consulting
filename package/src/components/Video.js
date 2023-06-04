0bc9eb0a99b5d47e42d411183047efa9562c1271
Reza <reza2011@gmail.com>
2023-06-04 18:37:18 -0400

    adding muted and preload attributes only to video

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -5,7 +5,7 @@ import VisionVideoMp4 from "../assets/images/vision.mp4";
 const Video = () => {
     return (
         <section className="vision">
-            <video className="vision__video" preload='auto' autoplay loop muted playsinline>
+            <video className="vision__video" muted preload="metadata">
                 <source src={VisionVideoMp4} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
