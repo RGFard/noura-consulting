fd8de143f24490dd0986b236751bb882aa8da3cb
Reza <reza2011@gmail.com>
2023-06-04 19:55:54 -0400

    fixing blogs width for iphone

--- package/src/components/Video.js
+++ package/src/components/Video.js
@@ -5,7 +5,7 @@ import VisionVideoMp4 from "../assets/images/vision.mp4";
 const Video = () => {
     return (
         <section className="vision">
-            <video className="vision__video" controls preload='auto' autoplay loop muted playsInline>
+            <video className="vision__video" autoPlay muted loop playsInline>
                 <source src={VisionVideoMp4} type="video/mp4" />
                 Your browser is not supported! Use Chrome or Firefox.
             </video>
