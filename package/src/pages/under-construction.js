4d17cf6cff1db9be9f65ecb5f24ad7f8e65c3223
Reza <reza2011@gmail.com>
2023-06-11 17:20:39 -0400

    adjusting under construction page and top menu

--- package/src/pages/under-construction.js
+++ package/src/pages/under-construction.js
@@ -10,7 +10,7 @@ export default function UnderConstruction() {
         <Layout>
             <Seo pageTitle="Under Construction page" />
             <main className="template-1">
-                <section className="template-1--main">
+                <section>
                     <h1 className="heading-1 heading-1--dark">Coming Soon!</h1>
                     <h3 className="heading-3">Under Construction!</h3>
                     <img alt="Under Construction!" src={underConstructionImage} className="template-1--image" />
