eaea782f0a3363bd2b490cb1900bbbb27982ee7e
Reza <reza2011@gmail.com>
2023-06-15 14:06:55 -0400

    fixing video attributes

--- package/src/pages/index.js
+++ package/src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import "../sass/style.scss";
 import Layout from "../components/layout/Layout";
 import Seo from "../components/general/SEO";
-import Video from "../components/Video";
+import Video from "../components/general/Video";
 import Services from "../components/site/services/Services";
 import Blogs from "../components/site/blogs/Blogs";
 
