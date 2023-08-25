2b6e0edeff325e9a795273826e8e97b5ada9daf2
Reza <reza2011@gmail.com>
2023-08-25 12:59:23 -0400

    making minor changes

--- package/src/components/layout/footer/FooterItemList.js
+++ package/src/components/layout/footer/FooterItemList.js
@@ -8,11 +8,10 @@ const FooterItemList = ({ list = [], icons, type }) => {
     return (
         list.map((listItem, index) => {
             let item, tooltipItem, url;
-            console.log(listItem.url);
             url = (listItem.url === "/services" || listItem.url === "/blog"
                 || listItem.url === "/services/ethereum-applications-smart-contracts-and-web3"
                 || listItem.url === "/services/web-and-mobile-interface-for-ethereum-application"
-                || listItem.url === "/services/consolidated-apis") ?
+                || listItem.url === "/services/consolidated-apis-for-off-chain-operations") ?
                 listItem.url : "/under-construction";
             item = tooltipItem = listItem.name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
             if (type !== "icon") {
