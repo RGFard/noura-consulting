afb4a0cd74b3be798419d28f3441d6e71f80c0d9
Reza <reza2011@gmail.com>
2023-10-01 14:43:57 -0400

    whatever

--- package/src/components/general/ContactForm.js
+++ package/src/components/general/ContactForm.js
@@ -6,7 +6,8 @@ import CryptoJS from "crypto-js";
 import Button from "../general/Button";
 
 export const decrypt = (encryptedPhrase) => {
-    var bytes = CryptoJS.AES.decrypt(encryptedPhrase, "This is my very secure secret key Aramaic5297");
+    const secretPhrase = process.env.SECRET_PHRASE + "";
+    var bytes = CryptoJS.AES.decrypt(encryptedPhrase, secretPhrase);
     var originalPhrase = bytes.toString(CryptoJS.enc.Utf8);
     return originalPhrase;
 };
