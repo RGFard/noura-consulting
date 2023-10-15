9915d2ca71ab051309822ce7cb2b26d745ee5a75
Reza <reza2011@gmail.com>
2023-10-15 15:02:08 -0400

    replacing videos

--- package/src/pages/services.js
+++ package/src/pages/services.js
@@ -1,22 +1,18 @@
 import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
 
 import "../sass/style.scss";
 import Layout from "../components/layout/Layout";
 import Head from "../components/general/Head";
 import Services from "../components/site/services/Services";
+import Video from "../components/general/Video";
+import ServicesVideoMp4 from "../assets/videos/services.mp4";
 
 export default function ServicesPage() {
     return (
         <Layout>
             <Head pageTitle="Services Page" />
             <div className="template2">
-                <section className="template2__section--header" key="header">
-                    <div className="template2__section--header-text">
-                        Services
-                    </div>
-                    <StaticImage alt="Blog" src="../assets/images/services.jpg" class="template2__section--header-image" />
-                </section>
+                <Video src={ServicesVideoMp4} title="Services" dark={true} />
             </div>
             <Services footer={false} />
         </Layout>
