e36073e04ae29ef494f819940ff219f66b115168
Reza Fard <r_gfard@yahoo.ca>
2026-03-28 16:52:05 +0000

    Adding gif videos along with mp4

--- package/src/components/general/Video.js
+++ package/src/components/general/Video.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const Video = ({ src, title, dark }) => {
-    let className = dark === true ?
-        "template2__section--header-video template2__section--header-video--dark" :
-        "template2__section--header-video";
+    let className = dark === true
+        ? "template2__section--header-video template2__section--header-video--dark"
+        : "template2__section--header-video";
+
+    const isGif = src.toLowerCase().endsWith(".gif");
 
     return (
         <section className="template2__section--header" key="header">
             <div className="template2__section--header-text">
                 {title}
             </div>
-            <video className={className} autoPlay loop muted playsInline>
-                <source src={src} type="video/mp4" />
-                Your browser is not supported! Use Chrome or Firefox.
-            </video>
+
+            {isGif ? (
+                <img src={src} className={className} alt="gif content" />
+            ) : (
+                <video className={className} autoPlay loop muted playsInline>
+                    <source src={src} type="video/mp4" />
+                    Your browser is not supported! Use Chrome or Firefox.
+                </video>
+            )}
         </section>
     );
 };
